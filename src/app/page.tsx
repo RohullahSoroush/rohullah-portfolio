14c7c21f81305f2ffd1e3395ae7b97f895fcfc8c
Rohullah Mohammdi <rohullahsoroush308@gmail.com>
2024-10-05 21:08:21 +0430

    started working on hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import HeroSection from "@/components/sections/HeroSection";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <h1>Coming soon....❤️</h1>
+      <HeroSection />
     </div>
   );
 }
