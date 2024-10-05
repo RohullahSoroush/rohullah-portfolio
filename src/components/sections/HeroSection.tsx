90c80865653885c34c7188ab6089a84eb8f4b6ce
Rohullah Mohammdi <rohullahsoroush308@gmail.com>
2024-10-05 21:16:54 +0430

    worked on hero section

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,16 +1,17 @@
+import Image from "next/image";
 import React from "react";
 
 const HeroSection = () => {
   return (
     <div>
       <div className="h-screen w-full mx-auto py-20 xl:px-16 xs:px-8 flex md:flex-row xs:flex-col gap-4 justify-center items-center pb-10 pt-4">
-        <div className="w-full">
+        {/* <div className="w-full">
           <img
             className="rounded-full md:max-w-[70%] sm:max-w-[50%] xs:max-w-[60%] mx-auto"
             src="https://aseel-medusa.s3.us-east-1.amazonaws.com/images/file_01J3FPX7XZC8V7V4RSTQYDARCB.png"
             alt="My Image"
           />
-        </div>
+        </div> */}
         <div className="w-full flex flex-col justify-center gap-4 text-white dark:text-gray-800 md:mt-0 sm:mt-8 xs:mt-4">
           <h1 className="text-4xl font-semibold font-serif">
             Hello, I'm Rohullah Mohammadi
