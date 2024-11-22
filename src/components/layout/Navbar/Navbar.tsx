b1de6b25a6e485502fa3bbcf7d31b34a17b9a3c0
Rohullah Mohammdi <rohullahsoroush308@gmail.com>
2024-11-22 21:49:46 +0430

    worked on Nav bar.

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,7 +1,17 @@
+import NavLink from "@/components/Buttons/NavLink";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-center mt-4">
+      <div className="flex gap-4 bg-[#009ebb] px-6 py-3 rounded-3xl text-white shadow-xl">
+        <NavLink href="/">Home</NavLink>
+        <NavLink href="/about">About</NavLink>
+        <NavLink href="/projects">Projects</NavLink>
+        <NavLink href="/contact">Contact</NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
