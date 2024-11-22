896aaafaa193001d3c0e21f5b193efcc289fe006
Rohullah Mohammdi <rohullahsoroush308@gmail.com>
2024-11-22 21:54:03 +0430

    made the nav bar stikcy

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center mt-4">
-      <div className="flex gap-4 bg-[#009ebb] px-6 py-3 rounded-3xl text-white shadow-xl">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/about">About</NavLink>
-        <NavLink href="/projects">Projects</NavLink>
-        <NavLink href="/contact">Contact</NavLink>
+    <div className="sticky left-0 top-4">
+      <div className="flex justify-center mt-4">
+        <div className="flex gap-4 bg-[#009ebb] px-6 py-3 rounded-3xl text-white shadow-xl">
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="/about">About</NavLink>
+          <NavLink href="/projects">Projects</NavLink>
+          <NavLink href="/contact">Contact</NavLink>
+        </div>
       </div>
     </div>
   );
